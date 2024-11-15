9a3ebcd5506db00ca54b7da62d5afb98535798bf
Daniil Komov <daniil.komov@student.lut.fi>
2024-11-15 23:02:28 +0200

    Added thread initialization & file data mapping

--- pzip.c
+++ pzip.c
@@ -2,6 +2,25 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <string.h>          //importing libraries
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <pthread.h>
+
+
+pthread_mutex_t lock;  //mutex lock mechanism, for handling multiple thread race conditions, for example when few threads are trying to access same code
+
+
+
+
+
+typedef struct {          //initializing certain data structure for each thread, with start of data segment, its length, and thread index
+    char *seg_start;
+    size_t seg_length;
+
+    size_t th_index; 
+} Th_data;
+
 
 
 
@@ -12,26 +31,73 @@ void message_printer(const char *msg) {      //Function for printing errors and 
 
 
 
-int main(int arg_counter, char *arg_select[]) {
+
+
+
+
+
+
+int main(int arg_counter, char *arg_select[]) {   //establishing the main function
 
     
     
     if (arg_counter < 2) {           //Handling incorrect usage cases by throwing usage suggestion
 
-        message_printer("usage: pzip <file1> <file2> ... > <outputFile>");
+        message_printer("usage: pzip <file1> <file2> ... > <outputFile>");  //calling message printer to print correct usage
 
 
-        exit(1);
+        exit(1);   //exiting the program
     }
 
 
 
 
-    for (int i = 1; i < arg_counter; i++) {
+    //Next step is to establish the threads, doing it based on the number of processors system has
+
+    int th_number  = sysconf(_SC_NPROCESSORS_ONLN);   //getting number of threads by getting the number of processors of the system. Using sysconf function for this purpose.
+
+
+    if (th_number < 1) {   //in case sysconf fails, we set the number of threads to 1 as default number (1 thread)
+        th_number = 1; 
+    }
+
+
+    pthread_t threads[th_number];   //Initializing the threads based on the established amount
+
+    Th_data th_data[th_number];    //Establishing Th_data instances (amount of instances based on thread number). Th_data data structure has been initialized earlier.
+
+    
+    pthread_mutex_init(&lock, NULL);   //Establishing mutex lock
+
+
+
+
+
+
+
+
+
+    for (int i = 1; i < arg_counter; i++) {     //establishing a loop for all the arguments (input files)
         
-        int fd = open(arg_select[i], O_RDONLY);
+        int input_file = open(arg_select[i], O_RDONLY);  //opening the file (one by one as it's a loop)
 
 
+
+
+        struct stat file_stats;   //initializing instance for collecting file statistics (for example size)
+
+
+
+        char *data = mmap(NULL, file_stats.st_size, PROT_READ, MAP_PRIVATE, input_file, 0);  //mapping file data into memory for easy access 
+
+
+
+
+
+
+
+
+        close(input_file);  //closing input file
     }
 
 
@@ -60,7 +126,7 @@ int main(int arg_counter, char *arg_select[]) {
 
 
 
-
+    pthread_mutex_destroy(&lock); //destroying mutex lock before exiting
 
     return 0;
 
